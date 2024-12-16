7954fe6dbb1c78386ad1a8673ddf2f8fbbbf3bd1
hashbrun554 <hashbrun444@gmail.com>
2024-12-16 10:38:59 -1000

    Initial Commit

---
+++ ZeroScreenTests/ZeroScreenTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZeroScreenTests.swift
+//  ZeroScreenTests
+//
+//  Created by Cristian Matache on 12/16/24.
+//
+
+import Testing
+@testable import ZeroScreen
+
+struct ZeroScreenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
